ddd1f5e7e59d531d05aac58039cc3ef20c5cefad
ghu0101exe <g.huy0102005@gmail.com>
2024-10-17 15:42:38 +0700

    khoi tao thu vien chua cac lop user va fish

---
+++ KoiShop/Libs/Shop_Fish/Fish.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libs.Shop_Fish
+{
+    internal class Fish
+    {
+        //public string Origin;
+        //public string Sex;
+        //public string Age;
+        public Fish() { }
+
+        public Fish(string Origin, string Sex, string Age)
+        {
+            this.Origin = Origin;
+            this.Sex = Sex;
+            this.Age = Age;
+        }
+
+        private string Origin { get; set; }
+        public string getOrigin() { return Origin; }
+        public void setID(String Origin) { this.Origin = Origin; }
+        public string Sex { get; set; }
+        public string getName() { return Sex; }
+        public void setName(String Sex) { this.Sex = Sex; }
+        public string Age { get; set; }
+        public string getPass() { return Age; }
+        public void setPass(String Age) { this.Age = Age; }
+    }
+}
---
+++ KoiShop/Libs/Shop_User/IUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libs.Shop_User
+{
+    public interface IUser
+    {
+        User createUser(string Id, string Name, string Pass, string userCase);
+        User createUser(string userCase);
+    }
+}
