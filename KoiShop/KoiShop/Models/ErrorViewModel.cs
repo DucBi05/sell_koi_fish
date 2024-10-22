648b8c61a726c85a73c7e35c8204d98d97008105
DucBi05 <bind2581@ut.edu.vn>
2024-10-23 01:58:02 +0700

    Delete KoiShop directory

--- KoiShop/KoiShop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace KoiShop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
