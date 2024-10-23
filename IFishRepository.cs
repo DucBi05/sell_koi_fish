365f0b8c21d66e841b5a1fac5487e2d6520bcd08
DucBi05 <bind2581@ut.edu.vn>
2024-10-23 23:59:11 +0700

    Commit code 3layer

---
+++ IFishRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KoiShop.Repositories.Entities;
+
+namespace KoiShop.Repositories.Interface
+{
+    public interface IFishRepository
+    {
+        Task<List<Fish>> GetAllFish();
+    }
+}
